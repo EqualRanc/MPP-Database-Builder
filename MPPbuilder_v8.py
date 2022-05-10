67fe0ec9c276c0d9c69b9da0ba5be536b5be0c5b
EqualRanc <92188554+EqualRanc@users.noreply.github.com>
2022-05-10 16:33:52 -0700

    Update MPPbuilder_v8.py

--- MPPbuilder_v8.py
+++ MPPbuilder_v8.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 20 16:00:04 2019
+Created on Tue May 10 16:19:04 2022
 
-@author: CRanque
+@author: EqualRanc
 """
 
-#import pdb    
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 16:00:04 2019
+
+@author: EqualRanc
+"""
+
 import pandas as pd
 import pubchempy as pcp
 import cirpy as cir
@@ -25,27 +31,35 @@ def pubsearch(casnum):
         except:
             p = pd.DataFrame([['notfound','notfound', 'notfound', 'notfound', 'notfound']], columns=['InChIKey','IUPACName','MolecularFormula','MonoisotopicMass','CanonicalSMILES'])
             return p
-        
-infile = r'D:\Python Scripts\Work\CAS - Short.csv'
-outfile = '%s - PubChem Search Results.xlsx' % infile[:-4]
-chem_df = pd.read_csv(infile)
-chem_df_num = chem_df.to_numpy()
-cas = []
-data = pd.DataFrame(data=None, columns=['InChIKey','IUPACName','MolecularFormula','MonoisotopicMass','CanonicalSMILES'])
-for casno in chem_df_num[:,0]:
-    cas.append(str(casno))
 
-#Parallel processing of CAS No. list for InChIKey, IUPAC Name, 
-#Molecular Formula, Monoisotopic Mass, and Canonical SMILES for processing MS data in MPP workflow (Agilent)
-t1 = process_time()
-with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:    
-    for item, casitem in zip(cas, executor.map(pubsearch, cas)):
-        data = pd.concat([data, casitem])
-t2 = process_time()
+def casimport(url):
+    url = "https://github.com/EqualRanc/MPP-Database-Builder/raw/main/CAS%20-%20Example%20Input%20File.csv"
+    outfile = '%s - PubChem Search Results.xlsx' % url[:-4]
+    chem_df = pd.read_csv(url)
+    chem_df_num = chem_df.to_numpy()
+    cas = []
+    data = pd.DataFrame(data=None, columns=['InChIKey','IUPACName','MolecularFormula','MonoisotopicMass','CanonicalSMILES'])
+    for casno in chem_df_num[:,0]:
+        cas.append(str(casno))
+    return cas, outfile, data
 
-print('PubChem Search Completed in minutes:', (t2-t1)/60)
+def cassearch(caslist,dat):
+    #Parallel processing of CAS No. list for InChIKey, IUPAC Name, 
+    #Molecular Formula, Monoisotopic Mass, and Canonical SMILES for processing MS data in MPP workflow (Agilent)
+    t1 = process_time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:    
+        for item, casitem in zip(caslist, executor.map(pubsearch, caslist)):
+            dat = pd.concat([dat, casitem])
+    t2 = process_time()
+    print('PubChem Search Completed in minutes:', (t2-t1)/60)
+    dat.index.names = ['CID']
+    return dat
 
-#pdb.set_trace()    
-data.index.names = ['CID']
-data.to_excel(outfile, index_label=True)
-print('Export to outfile %s completed' % outfile)
+def excelout(data):
+    data.to_excel(outfile, index_label=True)
+    print('Export to outfile %s completed' % outfile)
+    
+url = input('Enter URL to CAS No. CSV File: ')
+cas, outfile, data = casimport(url)
+data = cassearch(cas)    
+excelout(data)
